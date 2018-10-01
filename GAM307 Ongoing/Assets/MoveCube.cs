dbd97e45ba4490326a2736e7891ea9fac8e80ea4
Dakota  Mckenzie <DMckenzie@myjmc.edu.au>
2018-10-02 09:15:52 +1000

    GAM307 - Ongoing
    Test

---
+++ GAM307 Ongoing/Assets/MoveCube.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour {
+    public float speed = 5f;
+	// Use this for initialization
+	void Start () {
+        int rnd = Random.Range(0, 360);
+        transform.Rotate(new Vector3(0, rnd, 0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.Translate(new Vector3(0, 0, Time.deltaTime * speed));
+        if (Input.GetKey(KeyCode.W))
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+    }
+}
